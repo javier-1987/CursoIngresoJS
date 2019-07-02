adfdd11b671ef308dd2cee3ef2226447a834ca2c
javier-1987 <52429704+javier-1987@users.noreply.github.com>
2019-07-01 22:20:10 -0300

    Add files via upload
    archivos modificados ejercicios primera clase

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,10 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+    var nombre;
+    var edad;
+    nombre = document.getElementById("elNombre").value;// lo que esta escrito en la caja de texto se copia en la variable nombre
+    edad = document.getElementById("edad").value; // lo que esta escrito en la caja de texto se copia en la variable edad
+alert("usted se llama" + nombre+" y tiene" + edad + "años");
 }
 
